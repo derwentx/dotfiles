a14a9c12110026d367273e39541cc082178bc59f
derwentx <derwentx@gmail.com>
2017-07-12 09:25:15 +1000

    add atom.sh

---
+++ atom.sh
@@ -0,0 +1,17 @@
+# install languages
+apm install language-assembly language-elm language-haskell language-htaccess language-ini language-lisp language-lua language-supercollider language-x86asm
+
+# install linter, beautify & autocomplete
+pip install yapf
+brew install ctags beautysh
+apm install linter autocomplete autocomplete-plus set-syntax atom-beautify yapf atom-ctags ctags-status
+
+# install python stuff
+pip install --upgrade pylama pylama-pylint yapf autopep8
+apm install python-indent python-tools python-yapf linter-python autocomplete-python linter-pylama linter-pylint
+
+# install php stuff
+apm install linter-php atom-autocomplete-php
+
+# install workflow stuff
+apm install split-and-move dash script sort-lines todo change-case project-manager
