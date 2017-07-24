d869f0411b791410273d2f606dd761180716b5bc
derwentx <derwentx@gmail.com>
2017-07-24 15:41:38 +1000

    rewrite bootstrap multiline files array

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,27 +3,36 @@
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
+git submodule update --init --recursive;
+
+export files=(
+    .aliases
+    .bash_profile
+    .bash_prompt
+    .bashrc
+    .curlrc
+    .editorconfig
+    .exports
+    .functions
+    .gdbinit
+    .gvimrc
+    .pylintrc
+    .screenrc
+    .tmux.conf
+    .vimrc
+    .wgetrc
+    .zshrc
+)
 
 function doIt() {
-    for file in (
-        ".aliases",
-        ".bash_profile",
-        ".bash_prompt",
-        ".bashrc",
-        ".curlrc",
-        ".editorconfig",
-        ".exports",
-        ".functions",
-        ".gdbinit",
-        # ".gvimrc" # not finished yet
-        ".pylintrc",
-        ".screenrc",
-        ".tmux.conf",
-        ".zshrc",
-    )
-    if [[ -f "~/$file" || -L "~/$file" ]]; then
-        mv "~/$file" "~/$file.pre-dotfiles"
-    ln -s "$(pwd)/$file" "~/$file"
+    for file in "${files[@]}"
+    do
+        if [[ -f "$HOME/$file" || -L "$HOME/$file" || -d "$HOME/$file" ]]
+        then
+            mv "$HOME/$file" "$HOME/$file.pre-dotfiles"
+        fi
+        ln -s "$(pwd)/$file" "$HOME/$file"
+    done
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
