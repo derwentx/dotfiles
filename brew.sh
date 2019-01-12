9ba7aeeee72bc49fa57810fcb5ebc6774ddd2f34
derwentx <derwentx@gmail.com>
2019-01-12 12:12:09 +1100

    cherrypick 59d09e976e76e8f9adae9045d50816dec97f71f4

--- brew.sh
+++ brew.sh
@@ -40,9 +40,8 @@ brew install coreutils
 brew install moreutils
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`. To do so, run `sudo chsh -s /usr/local/bin/bash`.
+# running `chsh`.
 brew install bash
-brew tap homebrew/versions
 brew install bash-completion2
 
 # Install GNU grep
