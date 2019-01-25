8a08f3698f83bae00724d4334bae6c3fd94fcfac
Derwent <derwentx@gmail.com>
2019-01-26 07:58:47 +1100

    🐛 remove json function which breaks shell

--- shell/functions.sh
+++ shell/functions.sh
@@ -113,13 +113,13 @@ function gitio() {
 
 # Syntax-highlight JSON strings or files
 # Usage: `json '{"foo":42}'` or `echo '{"foo":42}' | json`
-function json() {
-	if [ -t 0 ]; then # argument
-		python -mjson.tool <<< "$*" | pygmentize -l javascript;
-	else # pipe
-		python -mjson.tool | pygmentize -l javascript;
-	fi;
-}
+# function json() {
+# 	if [ -t 0 ]; then # argument
+# 		python -mjson.tool < "$*" | pygmentize -l javascript;
+# 	else # pipe
+# 		python -mjson.tool | pygmentize -l javascript;
+# 	fi;
+# }
 
 # Run `dig` and display the most useful info
 function digga() {
