131fc508fa6745b2fbdc52dbbd1232357eaf98ea
Derwent <derwentx@gmail.com>
2019-01-26 11:04:49 +1100

    🎨 remove unused brew packages

--- init/brew.sh
+++ init/brew.sh
@@ -40,171 +40,213 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
-# Install Bash 4.
-brew install bash
-brew install bash-completion2
-
+# brew install bash-completion2
 # Install GNU grep
-brew tap homebrew/dupes
+brew install grep
+# brew tap homebrew/dupes 
+# homebrew/dupes was deprecated. This tap is now empty as all its formulae were migrated.
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 brew install the_silver_searcher
 brew install ack
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
+brew install gnu-sed
+brew install vim
 brew install screen
-brew install php
-brew install gmp
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
-
-# Setup ZSH Shell
-
+brew install wget
 brew install curl
-brew install zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 brew install git
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-chsh "${BREW_PREFIX}/bin/zsh"
-
-# Build tools
-brew install cmake bazel boost-build m4 automake autoconf
-
-# install python stuff
-brew install pyenv
-pip install --upgrade pylama pylama-pylint yapf autopep8 flake8
-
-# Ruby stuff
-brew install ruby rbenv ruby-build
-
-# Haskell stuff
-brew install stack
-stack setup
-brew install haskell-stack ghci
-stack install ghc-mod hlint stylish-haskell
-
-# Install Developer stuff
-brew install emacs tmux
-brew install ctags source-highlight
-brew cask install atom sublime-text3 macvim
-brew install macvim-askpass
-brew cask install iTerm2 github-desktop transmit dash
-brew install vimpager
-brew install gpg # for GPG signing commits
+brew install htop 
+brew install mtr 
+brew install archey 
+brew install ansifilter
 brew install git-lfs
-
-# Embedded Electronics
-brew cask install avrdude gcc-arm-embedded dfu-util libtool picocom
-brew install port
-sudo port install interceptty
-
-# Windows hacking
-brew install mingw-w64 wine winetricks virtualbox mono
-
-# Install web dev stuff
-brew install mysql npm node sqlparse
-brew cask install apache phpmyadmin sequel-pro
-
-# Install other CLI stuff
-brew install htop mtr archey ansifilter
-brew tap ravenac95/sudolikeaboss
-brew install sudolikeaboss
-
-# Browsers
-brew cask install google-chrome opera firefox vivaldi
-brew install lynx
-
-# Comms
-brew cask install discord slack skype
-
-# productivity
-brew cask install alfred cheatsheet dropbox synergy bettertouchtool
-
-# Finder extensions
-brew cask install path-finder qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv
-
-# Install other apps
-brew cask install disk-inventory-x exercism
-brew cask install google-drive etcher
-
-# Audio engineering
-brew cask install soundflower soundflowerbed milkytracker
-
-# Install OS Tweaking apps
-brew cask install gfxcardstatus istat-menus bartender hammerspoon
-
-# Have to install metasploit manually: https://www.darkoperator.com/installing-metasploit-framewor/
-brew services start postgresql
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install arp-scan
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install blueutil
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-brew install exploitdb
-# brew install fcrackzip
-# brew install foremost
-brew install gdb
-brew install gobuster
-# brew install hashpump
-brew install hydra
-brew install john-jumbo
-# brew install knock
-# brew install netpbm
-brew install nmap
-brew install ncrack
-brew install netcat
-brew install nikto
-brew install owasp-zsc
-brew cask install java owasp-zap
-brew install socat
-brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-brew cask install wireshark
-brew install wpscan
-
-# Install other useful binaries.
-brew install dark-mode
-brew install lua lua-jit
+brew install exa
+# brew tap ravenac95/sudolikeaboss
+# brew install sudolikeaboss
 brew install pv
-brew install rename
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
-brew install tree
-brew cask install tshark
-brew install vbindiff
+brew install tmux
 
-# images
-brew install exiv2
-brew install imagemagick --with-webp
-brew install webkit2png
-brew install pngcheck
-brew install xpdf
+# Crypto utils
+brew install gpg # for GPG signing commits
+brew install openssh
 
-# Compression
+# Compression utils
 brew install zopfli
 brew install xz
 brew install p7zip
 # brew install pigz
+
+# Install Bash 5.
+brew install bash
+# Switch to using brew-installed bash as default shell
+# if ! fgrep -q "$(brew --prefix bash)/bin/bash" /etc/shells; then
+#   sudo tee -a /etc/shells < <( echo "$(brew --prefix bash)/bin/bash" );
+#   chsh -s "$(brew --prefix bash)/bin/bash";
+# fi;
+
+# Setup ZSH Shell
+brew install zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "$(brew --prefix zsh)/bin/zsh" /etc/shells; then
+  sudo tee -a /etc/shells < <( echo "$(brew --prefix zsh)/bin/zsh" );
+  chsh -s "$(brew --prefix zsh)/bin/zsh";
+fi;
+
+# Build tools
+brew install cmake boost-build automake autoconf
+# brew install bazel
+# brew install m4
+
+# install python stuff
+brew install pyenv pyenv-virtualenv
+# Note: you may have to install zlib headers with command below to install python
+#  sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+# Python linters
+pip install --upgrade pip
+
+# Ruby stuff
+brew install ruby rbenv ruby-build
+
+# NodeJS Stuff
+brew install npm node
+
+# Linters
+pip install --upgrade pylama pylama-pylint yapf autopep8 flake8 yapf
+brew install ctags beautysh source-highlight
+brew install pandoc
+
+# Haskell stuff
+# brew install stack
+# stack setup
+# brew install haskell-stack ghci
+# stack install ghc-mod hlint stylish-haskell
+
+# Install Developer stuff
+brew install emacs
+brew install nvim
+brew cask install atom 
+# brew cask install sublime-text3 
+# brew cask install macvim
+# brew install macvim-askpass
+brew cask install visual-studio-code
+# brew cask install github-desktop
+brew cask install git-kraken
+brew cask install iTerm2
+# (paid apps)
+brew cask install transmit dash
+# brew install vimpager
+
+# Embedded Electronics
+# brew cask install avrdude gcc-arm-embedded dfu-util libtool picocom
+# brew install port
+# sudo port install interceptty
+
+# Windows hacking
+# brew install mingw-w64 wine winetricks virtualbox mono
+
+# Install web dev stuff
+# brew install php
+# brew install gmp
+# brew install mysql npm node sqlparse
+# brew cask install apache phpmyadmin sequel-pro
+
+# Browsers
+brew cask install firefox 
+# brew cask install google-chrome 
+# brew cask install opera 
+# brew cask install vivaldi
+# brew install lynx
+
+# Comms
+# brew cask install discord slack skype
+
+# productivity
+brew cask install alfred synergy bettertouchtool
+# brew cask install cheatsheet 
+
+# Cloud storage
+brew cask install dropbox google-drive
+
+# Finder
+brew cask install path-finder 
+# Quick-look extensions
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv
+
+# Install other apps
+brew cask install disk-inventory-x 
+# brew cask install exercism
+brew cask install balenaetcher
+
+# Audio engineering
+# brew cask install soundflower soundflowerbed milkytracker
+
+# Install OS Tweaking apps
+brew cask install gfxcardstatus hammerspoon keepingyouawake
+# (Paid apps)
+# brew cask install istat-menus bartender 
+# Android Notifications
+brew cask install noti
+
+# Essential hacking tools
+brew install arp-scan
+brew install nmap
+brew install speedtest_cli
+
+# More CTF tools; see https://github.com/ctfs/write-ups.
+# Have to install metasploit manually: https://www.darkoperator.com/installing-metasploit-framewor/
+# brew services start postgresql
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install blueutil
+# # brew install cifer
+# # brew install dex2jar
+# # brew install dns2tcp
+# brew install exploitdb
+# # brew install fcrackzip
+# # brew install foremost
+# brew install gdb
+# brew install gobuster
+# # brew install hashpump
+# brew install hydra
+# brew install john-jumbo
+# # brew install knock
+# # brew install netpbm
+# brew install ncrack
+# brew install netcat
+# brew install nikto
+# brew install owasp-zsc
+# brew cask install java owasp-zap
+# brew install socat
+# brew install sqlmap
+# # brew install tcpflow
+# # brew install tcpreplay
+# # brew install tcptrace
+# # brew install ucspi-tcp # `tcpserver` etc.
+# brew cask install wireshark
+# brew install wpscan
+# brew install testssl
+# brew cask install tshark
+
+# Install other useful binaries.
+# brew install dark-mode
+# brew install lua lua-jit
+# brew install rename
+# brew install ssh-copy-id
+brew install tree
+# brew install vbindiff
+
+# images
+# brew install exiv2
+# brew install imagemagick --with-webp
+# brew install webkit2png
+# brew install pngcheck
+# brew install xpdf
 
 
 # Remove outdated versions from the cellar.
